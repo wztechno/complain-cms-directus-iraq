3c4dc0b0c0b2a62d3c0fa616e832d29bff56092c
alihfala <92159810+alihfala@users.noreply.github.com>
2025-03-11 12:30:29 +0200

    changes

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TimelineTableRow from '@/components/TimelineTableRow';
+import { GrFilter } from 'react-icons/gr';
 
 async function getComplaints() {
   const res = await fetch('https://complaint.top-wp.com/items/ComplaintTimeline');
@@ -14,8 +15,20 @@ export default async function TimelinePage() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-8 mr-64">
+        <div className="flex justify-between items-center mb-8">
+            <h1 className="text-2xl font-bold mb-6 text-right">الحالة الزمنية للشكاوى</h1>
+            <div className="flex gap-4">
+                <button 
+                    className="h-10 w-10 hover:bg-[#4664AD] text-[#4664AD] hover:text-[#F9FAFB] p-2 rounded-lg bg-[#F9FAFB] flex items-center justify-center"
+                    >
+                    <GrFilter />
+                    </button>
+                    <button className="bg-[#4664AD] hover:bg-[#F9FAFB] hover:text-[#4664AD] text-[#F9FAFB] px-4 py-2 rounded-lg">
+                    تصدير البيانات
+                </button>
+            </div>
+        </div>
       <div className="bg-white rounded-lg shadow-md p-6">
-        <h1 className="text-2xl font-bold mb-6 text-right">الحالة الزمنية للشكاوى</h1>
         
         <div className="overflow-x-auto">
           <table className="w-full">
